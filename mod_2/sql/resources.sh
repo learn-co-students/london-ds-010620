4bb77cff15dd899d09107819b7597771f3ff6d78
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2020-01-23 09:52:34 +0000

    changed permission added tidy

--- mod_2/sql/resources.sh
+++ mod_2/sql/resources.sh
@@ -1,2 +1,3 @@
 open https://dataschool.com/how-to-teach-people-sql/sql-join-types-explained-visually/
+open https://cran.r-project.org/web/packages/tidyr/vignettes/tidy-data.html
 open https://sqlbolt.com/
